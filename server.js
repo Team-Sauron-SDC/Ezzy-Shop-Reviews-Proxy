395368061fd6a928e53cd0aa4efcbed3a0552886
carlitoswillis <carlitoswillis@berkeley.edu>
2020-06-05 15:08:31 -0700

    add quotes

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/:id', express.static(path.join(__dirname, 'public')));
 
-app.use('/product/:id', createProxyMiddleware({ target: process.env.INFO_URL, changeOrigin: true }));
-app.use('/product/shop/:shopId', createProxyMiddleware({ target:  process.env.INFO_URL, changeOrigin: true }));
-app.use('/product/colors/:id', createProxyMiddleware({ target:  process.env.INFO_URL, changeOrigin: true }));
-app.use('/api/reviews/:id', createProxyMiddleware({target: process.env.REVIEWS_URL, changeOrigin: true}))
-app.use('/api/carousel/:id', createProxyMiddleware({target: process.env.CAROUSEL_URL, changeOrigin: true }));
-app.use('/api/carouselEnlarged/:id', createProxyMiddleware({ target: process.env.CAROUSEL_URL, changeOrigin: true }));
-app.use('/products/:id', createProxyMiddleware({ target: process.env.SUGGESTED_URL, changeOrigin: true }));
-app.use('/get/random', createProxyMiddleware({ target: process.env.SUGGESTED_URL, changeOrigin: true }));
+app.use('/api/reviews/:id', createProxyMiddleware({ target: `${process.env.REVIEWS_URL}`, changeOrigin: true}))
+// app.use('/product/:id', createProxyMiddleware({ target: process.env.INFO_URL, changeOrigin: true }));
+// app.use('/product/shop/:shopId', createProxyMiddleware({ target: process.env.INFO_URL, changeOrigin: true }));
+// app.use('/product/colors/:id', createProxyMiddleware({ target: process.env.INFO_URL, changeOrigin: true }));
+// app.use('/api/carousel/:id', createProxyMiddleware({target: process.env.CAROUSEL_URL, changeOrigin: true }));
+// app.use('/api/carouselEnlarged/:id', createProxyMiddleware({ target: process.env.CAROUSEL_URL, changeOrigin: true }));
+// app.use('/products/:id', createProxyMiddleware({ target: process.env.SUGGESTED_URL, changeOrigin: true }));
+// app.use('/get/random', createProxyMiddleware({ target: process.env.SUGGESTED_URL, changeOrigin: true }));
 
 
 app.listen(PORT, (err) => {
